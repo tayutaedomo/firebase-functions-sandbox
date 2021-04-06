5658d6fca52cb8b26be1fe9a4320f651213e3881
tayutaedomo <tayutaedomo@gmail.com>
2021-04-07 01:45:23 +0900

    add: helloWorldGcf function for deploy test

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,8 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.helloWorldGcf = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
