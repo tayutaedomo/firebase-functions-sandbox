98c78339be089526c4394a26be0fe1406a6382af
tayutaedomo <tayutaedomo@gmail.com>
2021-04-07 01:52:40 +0900

    add: deploying region to function

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,13 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+//exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 
-exports.helloWorldGcf = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
+// exports.helloWorldGcf = (req, res) => {
+//   let message = req.query.message || req.body.message || 'Hello World!';
+//   res.status(200).send(message);
+// };
